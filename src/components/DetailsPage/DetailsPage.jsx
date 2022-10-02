c85339ac46cb919cdc2f8b11e52d5c1b190ea593
Mark Schumacher <schuma1022@gmail.com>
2022-10-02 09:41:46 +0100

    Style display of movie details

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,6 +1,10 @@
 import './DetailsPage.css';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 function DetailsPage() {
     const history = useHistory();
@@ -9,26 +13,29 @@ function DetailsPage() {
 
     return (
         <>
-            <h1>This is the details page</h1>
-            {console.log('detailsArray', detailsArray)}
-            <ul>
-                {
-                    detailsArray.map(movie => {
-                        return (
-                            <li key={movie.id}>
-                                <h3>{movie.title}</h3>
-                                <h3>{movie.description}</h3>
-                                <h3>{movie.poster}</h3>
-                                <h3>{movie.genres}</h3>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-
+            <Typography variant="h3" gutterBottom>Movie Details</Typography>
+            {
+                detailsArray.map(movie => {
+                    return (
+                        <Grid container spacing={2}>
+                            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <img src={movie.poster} width="275" height="400" />
+                            </Grid>
+                            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                                <Typography variant="h4" gutterBottom>{movie.title}</Typography>
+                                <Typography variant="body1" gutterBottom>{movie.description}</Typography>
+                                <br />
+                                <Typography variant="h6" gutterBottom>Genres</Typography>
+                                <Typography variant="body1" gutterBottom>{movie.genres}</Typography>
+                            </Grid>
+                        </Grid>
+                    )
+                })
+            }
             <button onClick={() => history.push('/')} >Back to list</button>
         </>
     );
 };
 
 export default DetailsPage;
+
